779a840035e0a4a87435cf3bbb693e969626445e
mahbubur-r <mahbubur-r@outlook.com>
2025-09-27 18:40:05 +0200

    added more funtionality in in-progress and resolved status bar

--- src/components/CustomerTickets/CustomerTickets.jsx
+++ src/components/CustomerTickets/CustomerTickets.jsx
@@ -5,7 +5,7 @@ const CustomerTicket = ({ pending, inProgress, resolved, onStart, onComplete, st
   return (
     <div className="border-2 border-red-500 max-w-[1600px] mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
       <div className="border-2 border-green-500 md:w-[1100px] col-span-1 md:col-span-2 bg-white p-2 rounded-xl shadow">
-        <h2 className="text-2xl font-semibold mb-4">Customer Tickets</h2>
+        <h2 className="text-2xl md:text-start text-center font-semibold md:mb-4">Customer Tickets</h2>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 border-2 border-red-500">
           {pending.length === 0 && (
@@ -24,9 +24,9 @@ const CustomerTicket = ({ pending, inProgress, resolved, onStart, onComplete, st
           : ""
       }
             >
-              <div className="flex justify-between items-start">
-                <h3 className="font-semibold text-gray-800">{ticket.title}</h3>
-                <span className="text-xs bg-green-100 text-green-800 px-2 py-1 rounded-full">
+              <div className="flex flex-col md:flex-row justify-between items-start">
+                <h3 className="font-semibold text-center text-gray-800">{ticket.title}</h3>
+                <span className="md:text-xs bg-green-100 text-green-800 px-2 py-1 rounded-full">
                   {ticket.status}
                 </span>
               </div>
@@ -35,13 +35,13 @@ const CustomerTicket = ({ pending, inProgress, resolved, onStart, onComplete, st
                 {ticket.description}
               </p>
 
-              <div className='flex justify-between items-center'>
-                <div className="flex items-center justify-between  space-x-4 text-sm text-gray-500 mt-4">
+              <div className='flex flex-col md:flex-row  justify-between items-center mt-2 md:mt-4'>
+                <div className="flex items-center justify-between  space-x-4 text-sm text-gray-500">
                 <span className="font-medium">#{ticket.id}</span>
                 <span className="uppercase text-sm font-semibold text-red-500">{ticket.priority}
                 </span>
               </div>
-              <div class="flex items-center justify-between space-x-4 mt-4">
+              <div class="flex items-center justify-between space-x-4">
                     <span>{ticket.customer}</span>
                     <span><i class="fa-regular fa-calendar"></i> {ticket.createdAt}</span>
             </div>
@@ -57,13 +57,13 @@ const CustomerTicket = ({ pending, inProgress, resolved, onStart, onComplete, st
         {inProgress.length === 0 ? (
           <p className="text-sm text-gray-500 mb-4">No in-progress tasks</p>
         ) : (
-          <div className="space-y-3 mb-4">
+          <div className="space-y-3 md:mb-4">
             {inProgress.map(t => (
               <div key={t.id} className="bg-white p-4 rounded-lg shadow-sm border">
                 <div className="font-medium text-gray-800">{t.title}</div>
                 <button
                   onClick={() => onComplete(t.id)}
-                  className="mt-3 w-full px-3 py-2 rounded-md bg-green-600 text-white font-medium hover:opacity-95 transition cursor-pointer"
+                  className="md:mt-3 w-full px-3 py-2 rounded-md bg-green-600 text-white font-medium hover:opacity-95 transition cursor-pointer"
                 >
                   Complete
                 </button>
@@ -72,7 +72,7 @@ const CustomerTicket = ({ pending, inProgress, resolved, onStart, onComplete, st
           </div>
         )}
 
-        <h4 className="text-lg font-semibold mt-6 mb-3">Resolved Task</h4>
+        <h4 className="text-lg font-semibold md:mt-6 md:mb-3">Resolved Task</h4>
         {resolved.length === 0 ? (
           <p className="text-sm text-gray-500">No resolved tasks yet</p>
         ) : (
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,31 +2,31 @@ import React from 'react';
 
 const Footer = () => {
     return (
-    <div className='border-2 border-red-500 text-white bg-[#000000] mt-10 max-w-[1600px] h-[430px] mx-auto'>
-     <div className=' flex justify-around'>
-            <div className='w-[350px] mt-10'>
-                <h1 className='font-bold'>CS — Ticket System</h1>
+    <div className='border-2 border-red-500 text-white bg-[#000000] md:mt-10 md:max-w-[1600px] md:h-[430px] mx-auto'>
+     <div className=' flex flex-col md:flex-row md:justify-around ml-10 items-stretch'>
+            <div className='w-[350px] mt-3 md:mt-10'>
+                <h1 className='font-bold md:text-start text-center'>CS — Ticket System</h1>
                  <p className='text-[#a1a1aa]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             </div>  
-        <div className='footer-card w-[120px] mt-10'>
+        <div className='footer-card w-[120px] mt-3 md:mt-10'>
             <h2 className='font-bold'>Company</h2>
             <p className='font-light text-[#a1a1aa] text-sm mt-2'>About Us</p>
             <p className='font-light text-[#a1a1aa] text-sm mt-2'>Our Mission</p>
             <p className='font-light text-[#a1a1aa] text-sm mt-2'>Contact Saled</p>
         </div>
-        <div className='footer-card w-[120px] mt-10'>
+        <div className='footer-card w-[120px] mt-3 md:mt-10'>
             <h2 className='font-bold'>Services</h2>
             <p className='font-light text-[#a1a1aa] text-sm mt-2'>Products & Services</p>
             <p className='font-light text-[#a1a1aa] text-sm mt-2'>Customer Stories</p>
             <p className='font-light text-[#a1a1aa] text-sm mt-2'>Download Apps</p>
         </div>
-        <div className='footer-card w-[120px] mt-10'>
+        <div className='footer-card w-[120px] mt-3 md:mt-10'>
             <h2 className='font-bold'>Information</h2>
             <p className='font-light text-[#a1a1aa] text-sm mt-2'>Privacy Policy</p>
             <p className='font-light text-[#a1a1aa] text-sm mt-2'>Terms & Conditions</p>
             <p className='font-light text-[#a1a1aa] text-sm mt-2'>Join Us</p>
         </div>
-        <div className='footer-card w-[170px] mt-10'>
+        <div className='footer-card w-[170px] mt-3 md:mt-10'>
             <h2 className='font-bold'>Social Links</h2>
             <p className='font-light text-[#a1a1aa] text-sm mt-2'>
                 <i class="fa-brands fa-x-twitter"></i> @CS — Ticket System</p>
@@ -38,9 +38,9 @@ const Footer = () => {
                 <i class="fa-regular fa-envelope"></i> support@cst.com</p>
         </div>
      </div>
-        <div className='mt-20 h-[100px]'>
+        <div className='mt-3 md:mt-20 h-[100px]'>
             <div class="border-t border-gray-700 max-w-[1600px] mx-auto"></div>
-             <p className='text-[#fafafa] font-light text-center mt-10 py-3'>© 2025 CS — Ticket System. All rights reserved.</p>
+             <p className='text-[#fafafa] font-light text-center mt-2 md:mt-10 py-3'>© 2025 CS — Ticket System. All rights reserved.</p>
         </div>
     </div>
     );
