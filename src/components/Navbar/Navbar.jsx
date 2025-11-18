06f5a0e13f6385678b86a5c12f8c171437910dae
mahbubur-r <mahbubur-r@outlook.com>
2025-11-19 00:01:20 +0100

    updated the heading

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
     <div className="navbar bg-base-100 shadow-sm flex flex-col md:flex-row justify-between  md:max-w-[1600px] mx-auto">
-            <h2 className='font-bold text-lg text-black px-6'>CS — Ticket System</h2>
+            <h2 className='font-bold text-lg text-black px-6'>Support Now</h2>
   <     div className="navbar-center flex flex-col md:flex-row md:gap-4 md:px-6">
             <ul className="menu mx-auto menu-horizontal font-bold  px-1 gap-2 md:gap-4 ">
                 <li>Home</li>
